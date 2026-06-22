ad2d2d62bcdb36c68f5b7c60e0e6a09a683e556a
Mansi <singhmansi1510@gmail.com>
2026-06-23 01:05:51 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,18 +13,21 @@ app.use(express.json());
 app.use("/api", movieRoutes);
 app.use("/api/auth", authRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected ✅");
-})
-.catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
